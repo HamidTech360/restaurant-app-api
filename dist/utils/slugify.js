44145f8feb3b04370bcc0eee82d94a0aa2f55776
HamidTech <hammedowolabi2001@gmail.com>
2022-08-24 15:27:04 +0100

    fix bug

--- dist/utils/slugify.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.slugify = void 0;
-const slugify = (text) => text.toLowerCase().split(" ").join("-");
-exports.slugify = slugify;
